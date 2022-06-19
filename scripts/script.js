f987fd4dfd714572165c56b8765092e597246a6e
Andy <awilliamscomposing@gmail.com>
2022-06-18 19:23:35 -0700

    Starting login form test.

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+const finalPassword = document.querySelector("#finalPassword");
+const generateEl = document.querySelector("#generate");
+let userArr = [];
+
+const charTypeArr = {
+  uppercase: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+  lowercase: ["abcdefghijklmnopqrstuvwxyz"],
+  numbers: ["0123456789"],
+  specialCharacters: ["!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"],
+};
+
+function generatePool() {
+  const getCharCount = document.querySelector("#charQuantity").value;
+  const userArr = [];
+  if (document.getElementById("uppercaseCheck").checked) {
+    userArr.push(charTypeArr.uppercase);
+  }
+  if (document.getElementById("lowercaseCheck").checked) {
+    userArr.push(charTypeArr.lowercase);
+  }
+  if (document.getElementById("numbersCheck").checked) {
+    userArr.push(charTypeArr.numbers);
+  }
+  if (document.getElementById("specialCharCheck").checked) {
+    userArr.push(charTypeArr.specialCharacters);
+  }
+
+  const joinedUserArr = userArr.join("");
+  const userArrLength = joinedUserArr.length;
+
+  let result = "";
+  for (i = 0; i < getCharCount; i++) {
+    result += joinedUserArr.charAt(Math.floor(Math.random() * userArrLength));
+  }
+
+  document.getElementById("finalPassword").innerHTML = result;
+}
